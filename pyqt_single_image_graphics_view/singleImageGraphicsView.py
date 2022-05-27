8516477db6ceb893c4aae6eaac48d6f5fdeccb64
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-27 09:24:59 +0900

    Add new function (setAspectRatioMode)

--- pyqt_single_image_graphics_view/singleImageGraphicsView.py
+++ pyqt_single_image_graphics_view/singleImageGraphicsView.py
@@ -1,3 +1,4 @@
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QGraphicsScene, QGraphicsView
 
@@ -5,6 +6,7 @@ from PyQt5.QtWidgets import QGraphicsScene, QGraphicsView
 class SingleImageGraphicsView(QGraphicsView):
     def __init__(self):
         super().__init__()
+        self.__aspectRatioMode = Qt.KeepAspectRatio
         self.__initVal()
 
     def __initVal(self):
@@ -24,3 +26,12 @@ class SingleImageGraphicsView(QGraphicsView):
         self._scene = QGraphicsScene()
         self._item = self._scene.addPixmap(self._p)
         self.setScene(self._scene)
+        self.fitInView(self._item, self.__aspectRatioMode)
+
+    def setAspectRatioMode(self, mode):
+        self.__aspectRatioMode = mode
+
+    def resizeEvent(self, e):
+        if self._item:
+            self.fitInView(self._item, self.__aspectRatioMode)
+        return super().resizeEvent(e)
